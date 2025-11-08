fc68ef16e667aa4a055486d691b8f79471cbdc6b
Umer1363 <umerkhanm89@gmail.com>
2025-11-08 16:10:32 +0500

    Updated the name of all folder and removed the spaces

---
+++ Chap-01/examplecode.py
@@ -0,0 +1,6 @@
+import math
+
+def eg_task(size, result_container):
+
+    for i in range(size):
+        result_container.append(math.sqrt(i**2 + i))
--- examplecode.py
+++
@@ -1,6 +0,0 @@
-import math
-
-def eg_task(size, result_container):
-
-    for i in range(size):
-        result_container.append(math.sqrt(i**2 + i))
